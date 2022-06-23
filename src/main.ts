7883f8d3702de97d0dd6d5b2c8189bb67261438c
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-23 10:49:24 +0800

    most cases handled

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,48 @@
 import moment from "moment";
 
-let fromInputElement = document.getElementById("from") as HTMLInputElement;
-let toInputElement = document.getElementById("to") as HTMLInputElement;
-let dayDiffInputElement = document.getElementById(
-  "day-diff"
-) as HTMLInputElement;
+const from = document.getElementById("from") as HTMLInputElement;
+const to = document.getElementById("to") as HTMLInputElement;
+const dayDiff = document.getElementById("day-diff") as HTMLInputElement;
+const includeEnd = document.getElementById("include_end") as HTMLInputElement;
 
-fromInputElement.addEventListener("change", handleDate);
-toInputElement.addEventListener("change", handleDate);
-dayDiffInputElement.addEventListener("change", handleDiffDays);
-
+from.addEventListener("change", handleDate);
+to.addEventListener("change", handleDate);
+includeEnd.addEventListener("change", handleDate);
+dayDiff.addEventListener("change", handleDiff);
 
 // Set from and to dates to right format, then check if it's valid
 function handleDate() {
-  const from = moment(new Date(fromInputElement.value));
-  const to = moment(new Date(toInputElement.value));
-
-  let dayDiff = to.diff(from, "day");
-
-  checkIfValid(from, to, dayDiff);
+  const fromDate = moment(new Date(from.value));
+  const toDate = moment(new Date(to.value));
+  const calculatedDiff = toDate.diff(fromDate, "day");
+  const diff = parseDiff(calculatedDiff);
+  
+  checkIfValidThenReturnValue(fromDate, toDate, diff);
 }
 
-// check if dates input are not empty.If not return day difference result
-function checkIfValid(from: moment.Moment, to: moment.Moment, dayDiff: number) {
-  if (!to.isValid() || !from.isValid()) {
-    dayDiffInputElement.value = "";
-  } else {
-    dayDiffInputElement.value = Math.abs(dayDiff).toString();
+// include end date if input is checked
+function parseDiff(dayDiff: number): number {
+  if (includeEnd.checked) {
+    return dayDiff + 1;
+  }
+  return dayDiff;
+}
+
+// return day difference number result when dates are valid
+function checkIfValidThenReturnValue(
+  from: moment.Moment,
+  to: moment.Moment,
+  diff: number
+) {
+  if (to.isValid() && from.isValid()) {
+    dayDiff.valueAsNumber = Math.abs(diff);
   }
 }
 
 // get difference days number then display the dates taking account of the difference
-function handleDiffDays() {
-  const diffDayValue = dayDiffInputElement.value;
-
-  fromInputElement.value = moment().format("YYYY-MM-DD");
-  toInputElement.value = moment()
-    .subtract(diffDayValue, "days")
-    .format("YYYY-MM-DD");
+function handleDiff() {
+  let diff = parseDiff(dayDiff.valueAsNumber);
+ 
+  from.value = moment().format("YYYY-MM-DD");
+  to.value = moment().add(diff, "days").format("YYYY-MM-DD");
 }
